6706f92d1625ff860d31c6fe9cce6043adfd8184
Guanzhou Hu <huguanzhou123@sina.com>
2020-08-24 12:45:20 -0500

    Include my ocf-mf as submodule ocf

--- ocf/tests/unit/tests/header.c
+++
@@ -1,5 +0,0 @@
-/*
- * Copyright(c) 2012-2020 Intel Corporation
- * SPDX-License-Identifier: BSD-3-Clause-Clear
- */
-
--- ocf/tests/unit/tests/print_desc.h
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright(c) 2012-2020 Intel Corporation
- * SPDX-License-Identifier: BSD-3-Clause-Clear
- */
-
-#define print_test_description(description) print_message("[   DESC   ] %s\n", description);
