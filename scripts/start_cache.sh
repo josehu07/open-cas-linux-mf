605683a35b8232e2695e2b33ee893cb72a4b2a3f
kaiweitu <kaiweitu@node-0.kaiweitu-qv80102.spark-pg0.wisc.cloudlab.us>
2020-09-13 01:14:34 -0500

    Modify start_cache

--- scripts/start_cache.sh
+++ scripts/start_cache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo casadm -S --load -d /dev/sdc            # Start cache in WT mode as cache_id = 1
-# sudo casadm -A -d /dev/yyy -i 1       # Add a core device to cache 1
+# sudo casadm -A -d /dev/sdb -i 1       # Add a core device to cache 1
 sudo casadm -L                        # List current CAS status
-sudo casadm -M -i 1 -j 1
+sudo casadm -M -i 1 -j 1 -m tl
 sudo casadm -Q -i 1 -c mfwa
 sudo casadm -L
 
